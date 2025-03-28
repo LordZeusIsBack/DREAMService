a5b139e82ec77d261ee45bbae457cb06b4ca264b
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-03-29 00:55:01 +0530

    Added changed to the model(s)

--- api/models.py
+++ api/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 class Seller(User):
     profile_picture = models.ImageField(upload_to='pictures/seller', null=True, blank=True)
     business_name = models.CharField(max_length=100, editable=False)
-    phone_number = PhoneNumberField(max_length=10, unique=True, null=True, blank=True, editable=False)
+    phone_number = models.CharField(max_length=10, unique=True, null=True, blank=True, editable=False)
     is_verified = models.BooleanField(default=False, editable=False)
 
     def __str__(self): return self.business_name
@@ -14,7 +13,7 @@ class Seller(User):
 
 class Buyer(User):
     profile_picture = models.ImageField(upload_to='pictures/buyer', null=True, blank=True)
-    phone_number = PhoneNumberField(max_length=10, unique=True, null=True, blank=True, editable=False)
+    phone_number = models.CharField(max_length=10, unique=True, null=True, blank=True, editable=False)
     is_verified = models.BooleanField(default=False, editable=False)
 
     def __str__(self): return self.username
