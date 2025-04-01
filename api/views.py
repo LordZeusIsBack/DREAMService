28f5edf4b17336f973a9f50ed11db3a07748e7df
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-02 04:08:39 +0530

    Connected View

--- api/views.py
+++ api/views.py
@@ -3,18 +3,8 @@ from rest_framework.decorators import api_view
 from .serializer import SellerSerializer
 import api.models as models
 
-# Create your views here.
-@api_view(['GET'])
-def index(r):
-    return Response({'message': 'Hello, World!'})
 
-@api_view(['GET', 'POST'])
-def seller_models(r):
+@api_view(['GET'])
+def seller_model_data(r):
     if r.method == 'GET':
         return Response(SellerSerializer(models.Seller.objects.all(), many=True).data)
-    else:
-        serializer = SellerSerializer(data=r.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
