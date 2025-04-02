7efeb6d9a32d90a3cf7b7412aff3068bf54a0940
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-02 15:09:54 +0530

    Added route for seller deletion

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ import api.views as views
 urlpatterns = [
     path('view-seller-model', views.seller_model_data, name='seller_model_data'),
     path('update-seller-model', views.seller_model_update, name='seller_model_update'),
+    path('delete-seller-model', views.seller_model_delete, name='seller_model_delete'),
 ]
