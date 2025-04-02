59a6863dc0405f2a619b2e66c7f8b40dde41c8ef
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-02 19:12:15 +0530

    Changed route for seller data

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 import api.views as views
 
 urlpatterns = [
-    path('view-seller-model', views.seller_model_data, name='seller_model_data'),
-    path('update-seller-model', views.seller_model_update, name='seller_model_update'),
-    path('delete-seller-model', views.seller_model_delete, name='seller_model_delete'),
+    path('view-seller/<int:seller_id>', views.seller_model_data, name='seller_model_data'),
 ]
