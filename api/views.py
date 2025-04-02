b58ce756ccf42c7afada315b84f918947d655bdd
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-02 19:12:43 +0530

    Updated view for single person data

--- api/views.py
+++ api/views.py
@@ -1,37 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .serializer import SellerSerializer
+import api.serializer as serializer
 import api.models as models
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 
 
 @api_view(['GET'])
-def seller_model_data(r):
-    if r.method == 'GET':
-        return Response(SellerSerializer(models.Seller.objects.all(), many=True).data)
-
-@api_view(['PUT', 'PATCH'])
-def seller_model_update(r):
-    try: seller_id = r.data['id']
-    except KeyError: return Response({'status': 'error', 'data': 'Seller ID not provided'},
-                                     status=status.HTTP_400_BAD_REQUEST)
-    seller_obj = get_object_or_404(models.Seller, id=seller_id)
-    partial_update = r.method == 'PATCH'
-    serializer = SellerSerializer(seller_obj, data=r.data, partial=partial_update)
-    if serializer.is_valid():
-        serializer.save()
-        return Response({'status': 'success', 'data': serializer.data},
-                        status=status.HTTP_200_OK)
-    return Response({'status': 'error', 'data': serializer.errors},
-                        status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['DELETE'])
-def seller_model_delete(r):
-    try: seller_id = r.data['id']
-    except KeyError: return Response({'status': 'error', 'data': 'Seller ID not provided'},
-                                     status=status.HTTP_400_BAD_REQUEST)
-    seller_obj = get_object_or_404(models.Seller, id=seller_id)
-    seller_obj.delete()
-    return Response({'status': 'success', 'data': f'Seller with ID {seller_id} has been deleted'},
-                    status=status.HTTP_200_OK)
+def seller_model_data(r, seller_id):
+    seller = get_object_or_404(models.Seller, id=seller_id)
+    serial = serializer.SellerSerializer(seller)
+    return Response(serial.data)
