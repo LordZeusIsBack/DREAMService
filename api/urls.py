a82d9c2d144e5c29afd6021c54270cac7fc70a8e
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-04 00:10:17 +0530

    Added route for adding a new seller

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ import api.views as views
 urlpatterns = [
     path('view-seller/<int:seller_id>', views.seller_model_data, name='seller_model_data'),
     path('update-seller/<int:seller_id>', views.update_seller_model_data, name='seller_model_data_update'),
+    path('add-seller', views.add_new_user, name='add_seller'),
 ]
