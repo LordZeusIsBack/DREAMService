a8b7a5f2386b8936667a9b0a9956a7114eff97c0
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-04 00:05:18 +0530

    Added DRY Principal for redundancy view functions

--- api/views.py
+++ api/views.py
@@ -6,17 +6,26 @@ from rest_framework import status
 from django.shortcuts import get_object_or_404
 
 
+def process_serializer(serializer_class, data, instance=None, success_status=status.HTTP_200_OK, create_status=status.HTTP_201_CREATED):
+    serializer_instance = serializer_class(instance, data=data) if instance else serializer_class(data=data)
+    if serializer_instance.is_valid():
+        try:
+            serializer_instance.save()
+            return serializer_instance.data, (success_status if instance else create_status)
+        except Exception as e:
+            return {'error': str(e)}, status.HTTP_500_INTERNAL_SERVER_ERROR
+    return serializer_instance.errors, status.HTTP_400_BAD_REQUEST
+
 @api_view(['GET'])
 def seller_model_data(r, seller_id): return Response(serializer.SellerSerializer(get_object_or_404(models.Seller, id=seller_id)).data)
 
 @api_view(['PUT'])
 def update_seller_model_data(request, seller_id):
     seller = get_object_or_404(models.Seller, id=seller_id)
-    serializer_instance = serializer.SellerSerializer(seller, data=request.data, partial=True)
-    if serializer_instance.is_valid():
-        try:
-            serializer_instance.save()
-            return Response(serializer_instance.data, status=status.HTTP_200_OK)
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    return Response(serializer_instance.errors, status=status.HTTP_400_BAD_REQUEST)
+    data, resp_status = process_serializer(serializer.SellerSerializer, instance=seller, data=request.data)
+    return Response(data, status=resp_status)
+
+@api_view(['POST'])
+def add_new_user(request):
+    data, resp_status = process_serializer(serializer.SellerSerializer, data=request.data)
+    return Response(data, status=resp_status)
