25b8f9aada079f3cf5b337579ed58c605550187c
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-03 13:02:13 +0530

    Added view for seller data

--- api/views.py
+++ api/views.py
@@ -7,7 +7,4 @@ from django.shortcuts import get_object_or_404
 
 
 @api_view(['GET'])
-def seller_model_data(r, seller_id):
-    seller = get_object_or_404(models.Seller, id=seller_id)
-    serial = serializer.SellerSerializer(seller)
-    return Response(serial.data)
+def seller_model_data(r, seller_id): return Response(serializer.SellerSerializer(get_object_or_404(models.Seller, id=seller_id)).data)
