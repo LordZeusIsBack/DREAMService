d049eae9700fc071ca16ee51c6f23173207b4f5b
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-03 21:40:22 +0530

    Added view for updaing seller

--- api/views.py
+++ api/views.py
@@ -8,3 +8,15 @@ from django.shortcuts import get_object_or_404
 
 @api_view(['GET'])
 def seller_model_data(r, seller_id): return Response(serializer.SellerSerializer(get_object_or_404(models.Seller, id=seller_id)).data)
+
+@api_view(['PUT'])
+def update_seller_model_data(request, seller_id):
+    seller = get_object_or_404(models.Seller, id=seller_id)
+    serializer_instance = serializer.SellerSerializer(seller, data=request.data, partial=True)
+    if serializer_instance.is_valid():
+        try:
+            serializer_instance.save()
+            return Response(serializer_instance.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    return Response(serializer_instance.errors, status=status.HTTP_400_BAD_REQUEST)
