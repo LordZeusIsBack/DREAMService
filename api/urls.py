0fb10817eee3f38b799bf4717bebc1a4601191fd
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-04 03:30:13 +0530

    Added route pointing to requrired apps

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,6 @@
-from django.urls import path
-import api.views as views
+from django.urls import path, include
 
 urlpatterns = [
-    path('view-seller/<int:seller_id>', views.seller_model_data, name='seller_model_data'),
-    path('view-seller-data', views.model_data, name='seller_model_data'),
-    path('update-seller/<int:seller_id>', views.update_seller_model_data, name='seller_model_data_update'),
-    path('add-seller', views.add_new_user, name='add_seller'),
+    path('seller/', include('seller_data.urls')),
+    path('buyer/', include('buyer_data.urls'))
 ]
