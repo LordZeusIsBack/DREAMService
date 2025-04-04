f6c89ccc2127f9fa4fab09bf3523f44e667930f7
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-04 16:29:33 +0530

    Added view for complete seller data (Temporary)

--- seller_data/views.py
+++ seller_data/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+import seller_data.serializer as serializer
+import seller_data.models as models
 
 # Create your views here.
+@api_view(['GET'])
+def model_data(r): return Response(serializer.SellerSerializer(models.Seller.objects.all(), many=True).data)
