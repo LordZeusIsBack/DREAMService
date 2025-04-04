a48da389bf59cb054bf19e661824793c58d5353d
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-04 23:20:32 +0530

    Added route for adding new seller

--- seller_data/urls.py
+++ seller_data/urls.py
@@ -4,5 +4,6 @@ import seller_data.views as views
 urlpatterns = [
     path('view-seller-data', views.model_data, name='seller_model_data'),
     path('view-seller/<int:seller_id>', views.seller_data, name='seller_model_data'),
-    path('update-seller/<int:seller_id>', views.update_seller_data, name='update_seller_model_data'),
+    path('update-seller/<int:seller_id>', views.update_seller_data, name='update_seller_data'),
+    path('add-seller', views.add_seller, name='add_seller'),
 ]
