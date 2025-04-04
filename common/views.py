a367d3c95ec7140bc5fd05704beb1a3ee2224cb2
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-04 23:10:52 +0530

    Added common process serializer

--- common/views.py
+++ common/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+import rest_framework.status as status
 
 # Create your views here.
+def process_serializer(serializer_class, data, instance=None, success_status=status.HTTP_200_OK, create_status=status.HTTP_201_CREATED):
+    serializer_instance = serializer_class(instance, data=data) if instance else serializer_class(data=data)
+    if serializer_instance.is_valid():
+        try:
+            serializer_instance.save()
+            return serializer_instance.data, (success_status if instance else create_status)
+        except Exception as e:
+            return {'error': str(e)}, status.HTTP_500_INTERNAL_SERVER_ERROR
+    return serializer_instance.errors, status.HTTP_400_BAD_REQUEST
