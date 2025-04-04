400416e5753f70a7ba5571cf6d8530cf74ddf49e
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-04 23:11:29 +0530

    Added view for updating seller data

--- seller_data/urls.py
+++ seller_data/urls.py
@@ -4,4 +4,5 @@ import seller_data.views as views
 urlpatterns = [
     path('view-seller-data', views.model_data, name='seller_model_data'),
     path('view-seller/<int:seller_id>', views.seller_data, name='seller_model_data'),
+    path('update-seller/<int:seller_id>', views.update_seller_data, name='update_seller_model_data'),
 ]
