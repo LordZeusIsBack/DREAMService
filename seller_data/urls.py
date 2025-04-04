359d3e3bdf7c2020207a939673bd34d60d9d3ed5
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-04 16:38:24 +0530

    Added route for getting single seller object

--- seller_data/urls.py
+++ seller_data/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 import seller_data.views as views
 
 urlpatterns = [
-    path('view-seller-data', views.model_data, name='seller_model_data')
+    path('view-seller-data', views.model_data, name='seller_model_data'),
+    path('view-seller/<int:seller_id>', views.seller_data, name='seller_model_data'),
 ]
