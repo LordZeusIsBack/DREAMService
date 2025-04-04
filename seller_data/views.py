0b82bdb6bb70bccbfbc2259851877db19c43841f
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-04 23:34:37 +0530

    Added view for deleting user

--- seller_data/views.py
+++ seller_data/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import seller_data.serializer as serializer
 import seller_data.models as models
+import rest_framework.status as status
 from django.shortcuts import get_object_or_404
 from common.views import process_serializer
 
@@ -25,3 +26,10 @@ def update_seller_data(request, seller_id):
 def add_seller(r):
     data, resp_status = process_serializer(serializer.SellerSerializer, data=r.data)
     return Response(data, status=resp_status)
+
+@api_view(['DELETE'])
+def delete_user(r, seller_id):
+    obj = get_object_or_404(models.Seller, id=seller_id)
+    obj.is_deleted = True
+    obj.save()
+    return Response(status=status.HTTP_204_NO_CONTENT)
