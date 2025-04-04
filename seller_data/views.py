e4b56d911115ccdacf6dbc56f701d7fbcf2d0c5b
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-04 23:12:36 +0530

    Connected view for updation

--- seller_data/views.py
+++ seller_data/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 import seller_data.serializer as serializer
 import seller_data.models as models
 from django.shortcuts import get_object_or_404
+from common.views import process_serializer
 
 # Create your views here.
 @api_view(['GET'])
@@ -13,3 +14,9 @@ def seller_data(r, seller_id):
     obj = get_object_or_404(models.Seller, id=seller_id)
     serial_instance = serializer.SellerSerializer(obj)
     return Response(serial_instance.data)
+
+@api_view(['PUT'])
+def update_seller_data(request, seller_id):
+    seller = get_object_or_404(models.Seller, id=seller_id)
+    data, resp_status = process_serializer(serializer.SellerSerializer, instance=seller, data=request.data)
+    return Response(data, status=resp_status)
