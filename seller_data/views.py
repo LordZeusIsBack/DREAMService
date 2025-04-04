ca44adf7a7cf6967a3bb75fc96d655b2ef8d8548
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-04 16:37:59 +0530

    Added view for single seller

--- seller_data/views.py
+++ seller_data/views.py
@@ -2,7 +2,14 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import seller_data.serializer as serializer
 import seller_data.models as models
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 @api_view(['GET'])
 def model_data(r): return Response(serializer.SellerSerializer(models.Seller.objects.all(), many=True).data)
+
+@api_view(['GET'])
+def seller_data(r, seller_id):
+    obj = get_object_or_404(models.Seller, id=seller_id)
+    serial_instance = serializer.SellerSerializer(obj)
+    return Response(serial_instance.data)
