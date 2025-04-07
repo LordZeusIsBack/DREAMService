171a91b0a919b13c2c55b24466a7a61f98cdf96d
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-08 05:22:53 +0530

    Added view function for getting buyer information

--- buyer_data/views.py
+++ buyer_data/views.py
@@ -7,3 +7,6 @@ from rest_framework.response import Response
 # Create your views here.
 @api_view(['GET', 'HEAD'])
 def view_buyer_model(r): return Response(serializer.BuyerSerializer(models.Buyer.objects.all(), many=True).data)
+
+@api_view(['GET'])
+def buyer_data(r, buyer_email): return Response(serializer.BuyerSerializer(get_object_or_404(models.Buyer, email=buyer_email, is_deleted=False)).data)
