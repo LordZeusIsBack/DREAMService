054fc6b53ff96b29762a4e3ecb708efe91469f79
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-08 11:24:08 +0530

    Added route for resetting forgotton password

--- seller_data/urls.py
+++ seller_data/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('view-seller/<int:seller_id>', views.seller_data, name='seller_model_data'),
     path('update-seller/<int:seller_id>', views.update_seller_data, name='update_seller_data'),
     path('add-seller', views.add_seller, name='add_seller'),
-    path('delete-seller/<int:seller_id>', views.delete_seller, name='delete_seller')
+    path('delete-seller/<int:seller_id>', views.delete_seller, name='delete_seller'),
+    path('forgot-password/', views.forgot_password, name='reset_password'),
 ]
