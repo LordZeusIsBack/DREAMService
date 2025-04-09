10c6d3b82dbbc4ff2964da1d65d2fc3d2e8221af
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-10 02:17:04 +0530

    Changed routes to accept emails

--- seller_data/urls.py
+++ seller_data/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 import seller_data.views as views
 
 urlpatterns = [
-    path('view-seller/<int:seller_id>', views.seller_data, name='seller_model_data'),
-    path('update-seller/<int:seller_id>', views.update_seller_data, name='update_seller_data'),
+    path('view-seller/<str:seller_email>', views.seller_data, name='seller_model_data'),
+    path('update-seller/<str:seller_email>', views.update_seller_data, name='update_seller_data'),
     path('add-seller', views.add_seller, name='add_seller'),
-    path('delete-seller/<int:seller_id>', views.delete_seller, name='delete_seller')
+    path('delete-seller/<str:seller_email>', views.delete_seller, name='delete_seller')
 ]
