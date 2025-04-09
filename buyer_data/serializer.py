cb2cf68d87b05c3cabc04031e2b676521d9314e3
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-10 04:59:41 +0530

    Added Common Serializer to BuyerSerializer

--- buyer_data/serializer.py
+++ buyer_data/serializer.py
@@ -1,15 +1,35 @@
 from rest_framework import serializers
+from common.serializer import BaseUserSerializer
 import buyer_data.models as models
 
 
 class BuyerVerificationSerializer(serializers.ModelSerializer):
+    """
+    Serializer for buyer verification.
+    """
+    aadhaar_card = serializers.IntegerField()
+    pan_number = serializers.CharField()
     class Meta:
         model = models.BuyerVerification
         fields = ('aadhaar_number', 'pan_number')
 
 
-class BuyerSerializer(serializers.ModelSerializer):
-    verification = BuyerVerificationSerializer(source='buyerverification', read_only=True)
-    class Meta:
+class BuyerSerializer(BaseUserSerializer):
+    """
+    Serializer for buyer model.
+    """
+    verification = BuyerVerificationSerializer(source='buyerverification', read_only=False)
+    class Meta(BaseUserSerializer.Meta):
         model = models.Buyer
-        fields = ('id', 'first_name', 'last_name', 'email', 'phone_number', 'username', 'verification')
+        fields = ('id', 'first_name', 'last_name', 'email', 'phone_number', 'username', 'password', 'verification')
+
+    def create(self, validated_data):
+        """
+        Create a new buyer instance.
+        """
+        return self.create_user(
+            validated_data,
+            models.Buyer,
+            models.BuyerVerification,
+            'verification'
+        )
