6ace64283ace5835bf742ad20fef9e8b2a4d6f2f
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-11 06:13:03 +0530

    Added route for updating buyer data

--- buyer_data/urls.py
+++ buyer_data/urls.py
@@ -3,5 +3,6 @@ import buyer_data.views as views
 
 urlpatterns = [
     path('view-buyer-data', views.view_buyer_model, name='view_buyer_model'),
-    path('view-buyer/<str:buyer_email>', views.view_buyer_model, name='view_buyer_model'),
+    path('view-buyer/<str:buyer_username>', views.buyer_data, name='view_buyer'),
+    path('update-buyer/<str:buyer_username>', views.update_buyer_data, name='update_buyer'),
 ]
