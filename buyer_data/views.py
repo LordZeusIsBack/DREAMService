3e42341bae9ff62e9cc63d3c8f96b0d5b0a3774b
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-11 06:13:33 +0530

    Added view function for updating buyer data

--- buyer_data/views.py
+++ buyer_data/views.py
@@ -3,10 +3,17 @@ from rest_framework.decorators import api_view
 import buyer_data.models as models
 import buyer_data.serializer as serializer
 from rest_framework.response import Response
+from common.views import process_serializer
 
 # Create your views here.
 @api_view(['GET', 'HEAD'])
 def view_buyer_model(r): return Response(serializer.BuyerSerializer(models.Buyer.objects.all(), many=True).data)
 
 @api_view(['GET'])
-def buyer_data(r, buyer_email): return Response(serializer.BuyerSerializer(get_object_or_404(models.Buyer, email=buyer_email, is_deleted=False)).data)
+def buyer_data(r, buyer_username): return Response(serializer.BuyerSerializer(get_object_or_404(models.Buyer, username=buyer_username, is_deleted=False)).data)
+
+@api_view(['PUT', 'PATCH'])
+def update_buyer_data(r, seller_username):
+    seller = get_object_or_404(models.Buyer, username=seller_username, is_deleted=False)
+    data, resp_status = process_serializer(serializer.BuyerSerializer, r.data, instance=seller)
+    return Response(data, status=resp_status)
