305caaeeff382adfac9cf97584686bbffc4a793c
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-11 06:08:48 +0530

    Changed views to accept username

--- seller_data/views.py
+++ seller_data/views.py
@@ -8,11 +8,11 @@ from common.views import process_serializer
 
 # Create your views here.
 @api_view(['GET'])
-def seller_data(r, seller_email): return Response(serializer.SellerSerializer(get_object_or_404(models.Seller, email=seller_email, is_deleted=False)).data)
+def seller_data(r, seller_username): return Response(serializer.SellerSerializer(get_object_or_404(models.Seller, username=seller_username, is_deleted=False)).data)
 
 @api_view(['PUT', 'PATCH'])
-def update_seller_data(r, seller_email):
-    seller = get_object_or_404(models.Seller, email=seller_email, is_deleted=False)
+def update_seller_data(r, seller_username):
+    seller = get_object_or_404(models.Seller, username=seller_username, is_deleted=False)
     data, resp_status = process_serializer(serializer.SellerSerializer, r.data, instance=seller)
     return Response(data, status=resp_status)
 
@@ -22,8 +22,8 @@ def add_seller(r):
     return Response(data, status=resp_status)
 
 @api_view(['DELETE'])
-def delete_seller(r, seller_email):
-    obj = get_object_or_404(models.Seller, email=seller_email, is_deleted=False)
+def delete_seller(r, seller_username):
+    obj = get_object_or_404(models.Seller, username=seller_username, is_deleted=False)
     obj.is_deleted = True
     obj.save()
     return Response(status=status.HTTP_204_NO_CONTENT)
