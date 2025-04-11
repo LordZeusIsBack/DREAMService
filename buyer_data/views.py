dae58824edd8179c56b823f7ff3e2ee1d713b5ee
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-11 06:16:05 +0530

    Added view function for soft deleting a user

--- buyer_data/views.py
+++ buyer_data/views.py
@@ -4,6 +4,7 @@ import buyer_data.models as models
 import buyer_data.serializer as serializer
 from rest_framework.response import Response
 from common.views import process_serializer
+from rest_framework.status import HTTP_204_NO_CONTENT
 
 # Create your views here.
 @api_view(['GET', 'HEAD'])
@@ -13,7 +14,14 @@ def view_buyer_model(r): return Response(serializer.BuyerSerializer(models.Buyer
 def buyer_data(r, buyer_username): return Response(serializer.BuyerSerializer(get_object_or_404(models.Buyer, username=buyer_username, is_deleted=False)).data)
 
 @api_view(['PUT', 'PATCH'])
-def update_buyer_data(r, seller_username):
-    seller = get_object_or_404(models.Buyer, username=seller_username, is_deleted=False)
+def update_buyer_data(r, buyer_username):
+    seller = get_object_or_404(models.Buyer, username=buyer_username, is_deleted=False)
     data, resp_status = process_serializer(serializer.BuyerSerializer, r.data, instance=seller)
     return Response(data, status=resp_status)
+
+@api_view(['DELETE'])
+def delete_seller(r, buyer_username):
+    obj = get_object_or_404(models.Buyer, username=buyer_username, is_deleted=False)
+    obj.is_deleted = True
+    obj.save()
+    return Response(status=HTTP_204_NO_CONTENT)
