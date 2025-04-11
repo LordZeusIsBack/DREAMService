041ef608f2ba024452362d806a4a261375a13626
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-11 06:08:02 +0530

    Changed routes accept username

--- seller_data/urls.py
+++ seller_data/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 import seller_data.views as views
 
 urlpatterns = [
-    path('view-seller/<str:seller_email>', views.seller_data, name='seller_model_data'),
-    path('update-seller/<str:seller_email>', views.update_seller_data, name='update_seller_data'),
+    path('view-seller/<str:seller_username>', views.seller_data, name='seller_model_data'),
+    path('update-seller/<str:seller_username>', views.update_seller_data, name='update_seller_data'),
     path('add-seller', views.add_seller, name='add_seller'),
-    path('delete-seller/<str:seller_email>', views.delete_seller, name='delete_seller')
+    path('delete-seller/<str:seller_username>', views.delete_seller, name='delete_seller')
 ]
