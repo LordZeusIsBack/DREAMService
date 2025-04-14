6acea381d11752b48c29643953a02cd045ef6e25
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-15 05:09:14 +0530

    Added code for resetting password

--- common/views.py
+++ common/views.py
@@ -1,8 +1,8 @@
 import rest_framework.status as status
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.conf import settings
 from rest_framework.response import Response
 
@@ -40,4 +40,22 @@ def send_password_reset_email(user, frontend_url):
             fail_silently=False
         )
         return Response({'success': 'Password reset mail has been sent!'}, status=status.HTTP_200_OK)
+    except Exception: return status.HTTP_500_INTERNAL_SERVER_ERROR
+
+def reset_user_password(uid, token, new_password, user_model):
+    """
+    Resets the user's password after verifying the token.
+    :param uid: Base64-encoded user ID.
+    :param token: Secure token for password reset.
+    :param new_password: The new password to set.
+    :param user_model: The model class (e.g., Seller or Buyer).
+    """
+    try:
+        pk = force_str(urlsafe_base64_decode(uid))
+        user = user_model.objects.get(pk=pk)
+        if not default_token_generator.check_token(user, token): return Response({'error': 'Invalid token or expired link.'}, status=status.HTTP_400_BAD_REQUEST)
+        user.set_password(new_password)
+        user.save()
+        return Response({'success': 'Your Password has been successfully changed!'}, status=status.HTTP_200_OK)
+    except (TypeError, ValueError, OverflowError, user_model.DoesNotExist): return Response({'error': 'Invalid token or expired link.'}, status=status.HTTP_400_BAD_REQUEST)
     except Exception: return status.HTTP_500_INTERNAL_SERVER_ERROR
