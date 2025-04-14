68aa99b18fa0c23b7b3ee6bd1acc082fdcbb24f0
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-15 05:25:49 +0530

    Added route for fotgot password view

--- seller_data/urls.py
+++ seller_data/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('view-seller/<str:seller_username>', views.seller_data, name='seller_model_data'),
     path('update-seller/<str:seller_username>', views.update_seller_data, name='update_seller_data'),
     path('add-seller', views.add_seller, name='add_seller'),
-    path('delete-seller/<str:seller_username>', views.delete_seller, name='delete_seller')
+    path('delete-seller/<str:seller_username>', views.delete_seller, name='delete_seller'),
+    path('forgot-password/', views.forgot_password, name='forgot_seller_password'),
 ]
