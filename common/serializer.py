c40b416b63f5cd749be63503265498f5aaff6ceb
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-16 11:54:17 +0530

    Password reset serializer

--- common/serializer.py
+++ common/serializer.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
+import rest_framework.status as status
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.conf import settings
+from rest_framework.response import Response
 
 class BaseUserSerializer(serializers.ModelSerializer):
     """
@@ -38,3 +45,35 @@ class BaseUserSerializer(serializers.ModelSerializer):
         if new_password: instance.set_password(new_password)
         instance.save()
         return instance
+
+
+class PasswordResetRequestSerializer(serializers.Serializer):
+    """
+    Serializer for requesting a password reset email.
+    """
+    email = serializers.EmailField()
+
+    @staticmethod
+    def send_password_reset_email(user, frontend_url):
+        """
+        Sends a password reset email to the user with a secure token.
+        :param user: The user instance (Seller or Buyer).
+        :param frontend_url: The frontend URL where the reset link will redirect.
+        """
+        try:
+            token = default_token_generator(user)
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            reset_link = f"{frontend_url}/reset-password/{uid}/{token}/"
+            send_mail(
+                'Password Reset Request',
+                f'Hello {user.get_full_name()},\n\n'
+                f'You requested a password reset for your account. '
+                f'Please click the following link to set a new password:\n\n'
+                f'{reset_link}\n\n'
+                f'This link will expire in 24 hours.',
+                settings.DEFAULT_FROM_EMAIL,
+                [user.email],
+                fail_silently=False
+            )
+            return Response({'success': 'Password reset mail has been sent!'}, status=status.HTTP_200_OK)
+        except Exception as e: return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
