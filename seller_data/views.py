f6c294e7b7d4d72d1603c5b3abec6d2f482246ba
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-16 12:14:14 +0530

    Changed import alias

--- seller_data/views.py
+++ seller_data/views.py
@@ -5,22 +5,22 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 import seller_data.models as models
-import seller_data.serializer as serializer
+import seller_data.serializer as seller_serializer
 from common.views import process_serializer
 
 # Create your views here.
 @api_view(['GET'])
-def seller_data(r, seller_username): return Response(serializer.SellerSerializer(get_object_or_404(models.Seller, username=seller_username, is_deleted=False)).data)
+def seller_data(r, seller_username): return Response(seller_serializer.SellerSerializer(get_object_or_404(models.Seller, username=seller_username, is_deleted=False)).data)
 
 @api_view(['PUT', 'PATCH'])
 def update_seller_data(r, seller_username):
     seller = get_object_or_404(models.Seller, username=seller_username, is_deleted=False)
-    data, resp_status = process_serializer(serializer.SellerSerializer, r.data, instance=seller)
+    data, resp_status = process_serializer(seller_serializer.SellerSerializer, r.data, instance=seller)
     return Response(data, status=resp_status)
 
 @api_view(['POST'])
 def add_seller(r):
-    data, resp_status = process_serializer(serializer.SellerSerializer, data=r.data)
+    data, resp_status = process_serializer(seller_serializer.SellerSerializer, data=r.data)
     return Response(data, status=resp_status)
 
 @api_view(['DELETE'])
