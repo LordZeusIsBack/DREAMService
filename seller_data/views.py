b48f5d562b35594cd6abc0bacfba8bcc5db24aa6
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-19 21:37:32 +0530

    Refactored code according to serializer

--- seller_data/views.py
+++ seller_data/views.py
@@ -39,7 +39,9 @@ def forgot_password(r):
     email = serializer_instance.validated_data['email']
     try:
         seller = models.Seller.objects.get(email=email, is_deleted=False)
-        return serializer_instance.send_password_reset_email(seller, settings.FRONTEND_URL)
+        success, message = serializer_instance.send_password_reset_email(seller, settings.FRONTEND_URL)
+        if success: return Response({'success': message}, status=status.HTTP_200_OK)
+        return Response({'error': message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     except models.Seller.DoesNotExist: return Response({'success': 'If the email is registered, you will receive a password reset email.'}, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
@@ -47,4 +49,6 @@ def forgot_password(r):
 def reset_password(r):
     serializer_instance = common_serializer.PasswordResetConfirmSerializer(data=r.data)
     if not serializer_instance.is_valid(): return Response(serializer_instance.errors, status=status.HTTP_400_BAD_REQUEST)
-    return serializer_instance.reset_password(models.Seller)
+    success, message = serializer_instance.reset_password(models.Seller)
+    if success: return Response({'success': message}, status=status.HTTP_200_OK)
+    return Response({'error': message}, status=status.HTTP_400_BAD_REQUEST)
