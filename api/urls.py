0df1276a6121ec473b249d877f716c83c3aced96
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-20 22:05:14 +0530

    included estate urls

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('seller/', include('seller_data.urls')),
-    path('buyer/', include('buyer_data.urls'))
+    path('buyer/', include('buyer_data.urls')),
+    path('estate/', include('estate_data.urls'))
 ]
