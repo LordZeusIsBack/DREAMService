96a2dfb72bab529f1ed9784b581057d63bda4bb5
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-26 03:50:02 +0530

    Added url for a single estate

--- estate_data/urls.py
+++ estate_data/urls.py
@@ -1,3 +1,5 @@
-from django.urls import path
-
-urlpatterns = []
+from django.urls import path, include
+from estate_data import views
+urlpatterns = [
+    path('estate-details/<str:estate_slug>', views),
+]
