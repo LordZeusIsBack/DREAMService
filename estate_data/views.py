5fa4e37fd471aa0641d30edd5d0837409102b38e
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-26 05:26:15 +0530

    Added view function for single estate data

--- estate_data/views.py
+++ estate_data/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+import models
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from serializer import EstateSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def get_estate_data(r, estate_slug): return Response(EstateSerializer(get_object_or_404(models.Estate, slug=estate_slug)).data)
