833e2e8557d2294a6ea26b27ab4a36ca5c546d48
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-27 18:27:26 +0530

    Added view for adding a new estate

--- estate_data/views.py
+++ estate_data/views.py
@@ -1,9 +1,23 @@
 import models
-from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from serializer import EstateSerializer
+from rest_framework.parsers import MultiPartParser, FormParser
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.response import Response
+import rest_framework.status as status
 
 # Create your views here.
 @api_view(['GET'])
 def get_estate_data(r, estate_slug): return Response(EstateSerializer(get_object_or_404(models.Estate, slug=estate_slug)).data)
+
+@api_view(['POST'])
+@parser_classes([MultiPartParser, FormParser])
+def add_new_estate(r):
+    """
+    Add new estate data.
+    """
+    serializer = EstateSerializer(data=r.data, context={'request': r})
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
