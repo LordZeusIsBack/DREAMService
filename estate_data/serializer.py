66ad258a736ed1af6c184a2eae88da832502f69f
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-27 17:44:56 +0530

    Corrected inheritance class

--- estate_data/serializer.py
+++ estate_data/serializer.py
@@ -2,14 +2,14 @@ from rest_framework import serializers
 import estate_data.models as models
 
 
-class EstateImageSerializer(serializers.Serializer):
+class EstateImageSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(required=True)
     class Meta:
         model = models.EstateImage
         fields = ['image']
 
 
-class EstateSerializer(serializers.Serializer):
+class EstateSerializer(serializers.ModelSerializer):
     images = EstateImageSerializer(source='estateimage', many=True)
     class Meta:
         model = models.Estate
