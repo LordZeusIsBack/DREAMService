367919f64873155bb6a6ed0ad3e8061922ee95f9
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-27 17:47:46 +0530

    added route for adding a new estate addition

--- estate_data/urls.py
+++ estate_data/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from estate_data import views
 urlpatterns = [
-    path('estate-details/<str:estate_slug>', views),
+    path('estate-details/<str:estate_slug>', views.get_estate_data),
+    path('add-new-estate', views.add_new_estate),
 ]
