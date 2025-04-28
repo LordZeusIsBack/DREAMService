10b89d1e4ab90304a32f9e624746bda84c81207b
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-28 06:04:41 +0530

    Added route for updating estate data

--- estate_data/urls.py
+++ estate_data/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from estate_data import views
 urlpatterns = [
-    path('estate-details/<str:estate_slug>', views.get_estate_data),
-    path('add-new-estate', views.add_new_estate),
+    path('estate-details/<str:estate_slug>', views.get_estate_data, name='estate_details'),
+    path('add-new-estate', views.add_new_estate, name='add_new_estate'),
+    path('update-estate/<str:slug>', views.update_estate_data, name='update_estate_data'),
 ]
