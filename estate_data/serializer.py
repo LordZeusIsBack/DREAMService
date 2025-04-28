f65296b32ec094031f42f6142a5a5c072f9c4197
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-28 07:17:47 +0530

    Added nested writable for deletion of estate images

--- estate_data/serializer.py
+++ estate_data/serializer.py
@@ -3,19 +3,27 @@ import estate_data.models as models
 
 
 class EstateImageSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
     image = serializers.ImageField(required=True)
     class Meta:
         model = models.EstateImage
-        fields = ['image']
+        fields = ['id', 'image']
 
 
 class EstateSerializer(serializers.ModelSerializer):
     images = EstateImageSerializer(many=True, read_only=True)
+    images_to_delete = serializers.ListField(
+        child=serializers.IntegerField(),
+        write_only=True,
+        required=False
+    )
     class Meta:
         model = models.Estate
-        fields = ['seller', 'estate_name', 'estate_type', 'estate_price', 'status', 'slug', 'location', 'images']
+        fields = ['seller', 'estate_name', 'estate_type', 'estate_price', 'status', 'slug', 'location', 'images',
+                  'images_to_delete']
 
     def create(self, validated_data):
+        if 'images_to_delete' in validated_data: validated_data.pop('images_to_delete')
         estate = models.Estate.objects.create(**validated_data)
         request = self.context.get('request')
         if request and request.FILES:
@@ -24,6 +32,8 @@ class EstateSerializer(serializers.ModelSerializer):
         return estate
 
     def update(self, instance, validated_data):
+        images_to_delete = validated_data.pop('images_to_delete', None)
+        if images_to_delete: models.EstateImage.objects.filter(id__in=images_to_delete, estate=instance).delete()
         for attr, value in validated_data.items(): setattr(instance, attr, value)
         instance.save()
         request = self.context.get('request')
