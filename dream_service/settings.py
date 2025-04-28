52e0c4f549d6a0a0efc0eb9d89bd24fd24a21538
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-28 09:19:51 +0530

    Installed token authentication app

--- dream_service/settings.py
+++ dream_service/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework.authtoken"
     "api",
     "seller_data",
     "buyer_data",
