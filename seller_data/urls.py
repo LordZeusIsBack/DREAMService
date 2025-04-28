cf0aedad39d58f48bce21746051478b599ec0923
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-28 09:29:30 +0530

    Added API route for seller login

--- seller_data/urls.py
+++ seller_data/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('delete-seller/<str:seller_username>', views.delete_seller, name='delete_seller'),
     path('forgot-password/', views.forgot_password, name='forgot_seller_password'),
     path('reset-password/', views.reset_password, name='reset_seller_password'),
+    path('login/', views.seller_login, name='seller_login'),
 ]
