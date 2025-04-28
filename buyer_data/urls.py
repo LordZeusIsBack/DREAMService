459e9c6f65093e9717e386a0c0493573ae57cddf
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-28 15:02:45 +0530

    Added route for new buyer

--- buyer_data/urls.py
+++ buyer_data/urls.py
@@ -4,5 +4,6 @@ import buyer_data.views as views
 urlpatterns = [
     path('view-buyer/<str:buyer_username>', views.buyer_data, name='view_buyer'),
     path('update-buyer/<str:buyer_username>', views.update_buyer_data, name='update_buyer'),
+    path('add-buyer', views.add_buyer, name='add_buyer'),
     path('delete-buyer/<str:buyer_username>', views.delete_buyer, name='delete_buyer'),
 ]
