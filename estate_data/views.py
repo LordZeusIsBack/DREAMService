fed4544c8f9b72d15c65a7b4042893a75d0c018d
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-28 06:05:08 +0530

    Added function for updating the estate data

--- estate_data/views.py
+++ estate_data/views.py
@@ -1,7 +1,8 @@
 import models
 from serializer import EstateSerializer
-from rest_framework.parsers import MultiPartParser, FormParser
 from django.shortcuts import get_object_or_404
+from django.db import transaction
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.response import Response
 import rest_framework.status as status
@@ -21,3 +22,17 @@ def add_new_estate(r):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'PATCH'])
+@parser_classes([MultiPartParser, FormParser])
+def update_estate_data(r, slug):
+    """
+    Update estate data.
+    """
+    with transaction.atomic():
+        estate = get_object_or_404(models.Estate, slug=slug)
+        serializer = EstateSerializer(estate, data=r.data, context={'request': r}, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
