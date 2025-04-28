12c7d665a106f0fa2be454454b33ee82923f80e3
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-28 12:59:28 +0530

    Added a new field, agent rera id

--- seller_data/models.py
+++ seller_data/models.py
@@ -12,6 +12,7 @@ class Seller(User, UserExtensionMixin):
 
 class SellerVerification(VerificationMixin):
     seller = models.OneToOneField(Seller, on_delete=models.CASCADE)
+    agent_rera_id = models.CharField(max_length=12, unique=True, editable=False)
     gstin = models.BigIntegerField(validators=[MaxValueValidator(9999999999)], unique=True, editable=False)
 
     def __str__(self): return self.seller.business_name
