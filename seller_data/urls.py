2be2989ec605c47ab2d271463be9434f52485a58
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-29 14:06:00 +0530

    Corrected routes

--- seller_data/urls.py
+++ seller_data/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('update-seller/<str:seller_username>', views.update_seller_data, name='update_seller_data'),
     path('add-seller', views.add_seller, name='add_seller'),
     path('delete-seller/<str:seller_username>', views.delete_seller, name='delete_seller'),
-    path('forgot-password/', views.forgot_password, name='forgot_seller_password'),
-    path('reset-password/', views.reset_password, name='reset_seller_password'),
+    path('forgot-password/', views.seller_forgot_password, name='forgot_seller_password'),
+    path('reset-password/', views.seller_reset_password, name='reset_seller_password'),
     path('login/', views.seller_login, name='seller_login'),
 ]
