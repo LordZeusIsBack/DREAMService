c183ab8edfb7629668631ff5a4d6992329cb9601
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-29 21:51:16 +0530

    Added buyer login functionality

--- buyer_data/views.py
+++ buyer_data/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 import buyer_data.models as models
-import buyer_data.serializer as serializer
+import buyer_data.serializer as buyer_serializer
 from rest_framework.response import Response
 import common.views as common_views
 import common.serializer as common_serializer
@@ -10,18 +10,18 @@ from django.conf import settings
 
 # Create your views here.
 @api_view(['GET'])
-def buyer_data(r, buyer_username): return Response(serializer.BuyerSerializer(get_object_or_404(models.Buyer, username=buyer_username, is_deleted=False)).data)
+def buyer_data(r, buyer_username): return Response(buyer_serializer.BuyerSerializer(get_object_or_404(models.Buyer, username=buyer_username, is_deleted=False)).data)
 
 @api_view(['PUT', 'PATCH'])
 def update_buyer_data(r, buyer_username):
     obj = get_object_or_404(models.Buyer, username=buyer_username, is_deleted=False)
-    data, resp_status = common_views.process_serializer(serializer.BuyerSerializer, r.data, instance=obj)
+    data, resp_status = common_views.process_serializer(buyer_serializer.BuyerSerializer, r.data, instance=obj)
     return Response(data, status=resp_status)
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def add_buyer(r):
-    data, resp_status = common_views.process_serializer(serializer.BuyerSerializer, data=r.data)
+    data, resp_status = common_views.process_serializer(buyer_serializer.BuyerSerializer, data=r.data)
     return Response(data, status=resp_status)
 
 @api_view(['DELETE'])
@@ -34,3 +34,7 @@ def buyer_forgot_password(r): return common_views.handle_password_reset(r.data, 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def buyer_reset_password(r): return common_views.handle_password_reset_conformation(r.data, models.Buyer, common_serializer.PasswordResetConfirmSerializer)
+
+@api_view(['PUT', 'PATCH'])
+@permission_classes([AllowAny])
+def buyer_login(r): return common_views.handle_user_login(r.data, models.Buyer, buyer_serializer.BuyerSerializer, 'buyer')
