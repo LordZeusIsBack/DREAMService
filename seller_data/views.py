ff8dce64026d0af5795207767b53bd77941623b8
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-29 13:26:03 +0530

    Refactored code for deletion of seller

--- seller_data/views.py
+++ seller_data/views.py
@@ -9,7 +9,7 @@ from rest_framework.response import Response
 import seller_data.models as models
 import seller_data.serializer as seller_serializer
 import common.serializer as common_serializer
-from common.views import process_serializer
+import common.views as common_views
 
 # Create your views here.
 @api_view(['GET'])
@@ -18,20 +18,16 @@ def seller_data(r, seller_username): return Response(seller_serializer.SellerSer
 @api_view(['PUT', 'PATCH'])
 def update_seller_data(r, seller_username):
     seller = get_object_or_404(models.Seller, username=seller_username, is_deleted=False)
-    data, resp_status = process_serializer(seller_serializer.SellerSerializer, r.data, instance=seller)
+    data, resp_status = common_views.process_serializer(seller_serializer.SellerSerializer, r.data, instance=seller)
     return Response(data, status=resp_status)
 
 @api_view(['POST'])
 def add_seller(r):
-    data, resp_status = process_serializer(seller_serializer.SellerSerializer, data=r.data)
+    data, resp_status = common_views.process_serializer(seller_serializer.SellerSerializer, data=r.data)
     return Response(data, status=resp_status)
 
 @api_view(['DELETE'])
-def delete_seller(r, seller_username):
-    obj = get_object_or_404(models.Seller, username=seller_username, is_deleted=False)
-    obj.is_deleted = True
-    obj.save()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+def delete_seller(r, seller_username): return common_views.soft_delete_user(models.Seller, seller_username)
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
