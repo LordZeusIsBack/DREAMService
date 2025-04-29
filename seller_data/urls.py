a553f2ff1750fef7ead96d473e8a34ed639b3f3f
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-30 04:00:45 +0530

    Changed url names

--- seller_data/urls.py
+++ seller_data/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 import seller_data.views as views
 
 urlpatterns = [
-    path('view-seller/<str:seller_username>', views.seller_data, name='seller_model_data'),
-    path('update-seller/<str:seller_username>', views.update_seller_data, name='update_seller_data'),
+    path('view-seller/<str:seller_username>', views.seller_data, name='view_seller'),
+    path('update-seller/<str:seller_username>', views.update_seller_data, name='update_seller'),
     path('add-seller', views.add_seller, name='add_seller'),
     path('delete-seller/<str:seller_username>', views.delete_seller, name='delete_seller'),
-    path('forgot-password/', views.seller_forgot_password, name='forgot_seller_password'),
-    path('reset-password/', views.seller_reset_password, name='reset_seller_password'),
+    path('forgot-password/', views.seller_forgot_password, name='seller_forgot_password'),
+    path('reset-password/', views.seller_reset_password, name='seller_reset_password'),
     path('login/', views.seller_login, name='seller_login'),
 ]
