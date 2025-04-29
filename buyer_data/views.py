8b53d8f418d57693065332f3ee993f897e2db856
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-29 14:00:17 +0530

    corrected imports

--- buyer_data/views.py
+++ buyer_data/views.py
@@ -4,8 +4,9 @@ from rest_framework.permissions import AllowAny
 import buyer_data.models as models
 import buyer_data.serializer as serializer
 from rest_framework.response import Response
-from common.views import process_serializer
-from rest_framework.status import HTTP_204_NO_CONTENT
+import common.views as common_views
+import common.serializer as common_serializer
+from django.conf import settings
 
 # Create your views here.
 @api_view(['GET'])
@@ -14,13 +15,13 @@ def buyer_data(r, buyer_username): return Response(serializer.BuyerSerializer(ge
 @api_view(['PUT', 'PATCH'])
 def update_buyer_data(r, buyer_username):
     obj = get_object_or_404(models.Buyer, username=buyer_username, is_deleted=False)
-    data, resp_status = process_serializer(serializer.BuyerSerializer, r.data, instance=obj)
+    data, resp_status = common_views.process_serializer(serializer.BuyerSerializer, r.data, instance=obj)
     return Response(data, status=resp_status)
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def add_buyer(r):
-    data, resp_status = process_serializer(serializer.BuyerSerializer, data=r.data)
+    data, resp_status = common_views.process_serializer(serializer.BuyerSerializer, data=r.data)
     return Response(data, status=resp_status)
 
 @api_view(['DELETE'])
