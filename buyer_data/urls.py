cd402d613fbe9570cb238bea55a0490c049bbe7b
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-30 18:34:57 +0530

    Used common urls patterns for pattern

--- buyer_data/urls.py
+++ buyer_data/urls.py
@@ -1,11 +1,4 @@
 import buyer_data.views as views
+from common.urls import create_user_url_patterns
 
-urlpatterns = [
-    path('view-buyer/<str:buyer_username>', views.buyer_data, name='view_buyer'),
-    path('update-buyer/<str:buyer_username>', views.update_buyer_data, name='update_buyer'),
-    path('add-buyer', views.add_buyer, name='add_buyer'),
-    path('delete-buyer/<str:buyer_username>', views.delete_buyer, name='delete_buyer'),
-    path('forgot-password/', views.buyer_forgot_password, name='buyer_forgot_password'),
-    path('reset-password/', views.buyer_reset_password, name='buyer_reset_password'),
-    path('buyer-login/', views.buyer_login, name='buyer_login'),
-]
+urlpatterns = create_user_url_patterns(views, 'buyer')
