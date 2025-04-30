754ba03d5fcfceb04e29995e77458276730335dc
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-04-30 21:05:04 +0530

    Added common url pattern for seller_data

--- seller_data/urls.py
+++ seller_data/urls.py
@@ -1,11 +1,4 @@
 import seller_data.views as views
+from common.urls import create_user_url_patterns
 
-urlpatterns = [
-    path('view-seller/<str:seller_username>', views.seller_data, name='view_seller'),
-    path('update-seller/<str:seller_username>', views.update_seller_data, name='update_seller'),
-    path('add-seller', views.add_seller, name='add_seller'),
-    path('delete-seller/<str:seller_username>', views.delete_seller, name='delete_seller'),
-    path('forgot-password/', views.seller_forgot_password, name='seller_forgot_password'),
-    path('reset-password/', views.seller_reset_password, name='seller_reset_password'),
-    path('login/', views.seller_login, name='seller_login'),
-]
+urlpatterns = [create_user_url_patterns(views, 'seller')]
