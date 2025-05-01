d49c2939601fb3db3232fa51e2034d7c849ddbee
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-05-01 15:28:10 +0530

    Removrd Django Secret Key

--- dream_service/settings.py
+++ dream_service/settings.py
@@ -24,7 +24,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-37^wv@l^#)e$k0c_^cq_40gc@*beq=sw__6+)cih5am&$k-o0&"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
