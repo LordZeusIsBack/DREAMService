696e86683349218b996d5791a9b5eb91de777169
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-05-04 04:57:06 +0530

    Connected route to te area search view

--- estate_data/urls.py
+++ estate_data/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from estate_data import views
+from django.views.decorators.cache import cache_page
 urlpatterns = [
     path('estate-details/<str:estate_slug>', views.get_estate_data, name='estate_details'),
     path('add-new-estate', views.add_new_estate, name='add_new_estate'),
     path('update-estate/<str:slug>', views.update_estate_data, name='update_estate_data'),
+    path('area', cache_page(60*60)(views.EstateAreaView.as_view()), name='estate_area'),
 ]
