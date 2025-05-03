2315c399789e24182f7531d11b6e226472658f73
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-05-03 10:42:09 +0530

    Created Custom User Manager

--- common/models.py
+++ common/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
+class CustomUsermanager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email: raise ValueError('Email is required!')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, username, password, **extra_fields)
+
 class UserExtensionMixin(models.Model):
     is_deleted = models.BooleanField(default=False)
     profile_picture = models.ImageField(upload_to='pictures/seller', null=True, blank=True)
