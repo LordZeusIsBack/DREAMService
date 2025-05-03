37d196122cd0d3c34f54687ff3ff3f68c1fb3485
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-05-03 10:55:05 +0530

    Corrected Imports

--- seller_data/models.py
+++ seller_data/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator
-from common.models import UserExtensionMixin, VerificationMixin
+from common.models import CustomUser, UserExtensionMixin, VerificationMixin
 
 # Create your models here.
 class Seller(User, UserExtensionMixin):
