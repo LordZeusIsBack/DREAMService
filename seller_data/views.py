ef2bbc168fddaa74efdcb13898a7fac06133890b
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-05-03 18:32:19 +0530

    Corrected parameter model

--- seller_data/views.py
+++ seller_data/views.py
@@ -7,7 +7,7 @@ import seller_data.serializer as seller_serializer
 import common.views as common_views
 
 # Create your views here.
-seller_views = create_user_views(models.Seller, seller_serializer.SellerSerializer, 'seller')
+seller_views = common_views.create_user_views(CustomUser, seller_serializer.SellerSerializer, 'seller')
 
 delete_seller = seller_views['delete_user']
 seller_forgot_password = seller_views['forgot_password']
@@ -15,11 +15,11 @@ seller_reset_password = seller_views['reset_password']
 seller_login = seller_views['login']
 
 @api_view(['GET'])
-def seller_data(r, seller_username): return Response(seller_serializer.SellerSerializer(get_object_or_404(models.Seller, username=seller_username, is_deleted=False)).data)
+def seller_data(r, seller_username): return Response(seller_serializer.SellerSerializer(get_object_or_404(Seller, username=seller_username, is_deleted=False)).data)
 
 @api_view(['PUT', 'PATCH'])
 def update_seller_data(r, seller_username):
-    seller = get_object_or_404(models.Seller, username=seller_username, is_deleted=False)
+    seller = get_object_or_404(Seller, username=seller_username, is_deleted=False)
     data, resp_status = common_views.process_serializer(seller_serializer.SellerSerializer, r.data, instance=seller)
     return Response(data, status=resp_status)
 
