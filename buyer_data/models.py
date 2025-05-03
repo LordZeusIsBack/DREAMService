14fb388ab63149fa83e2ca7678cc5b8f970565a9
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-05-03 10:54:24 +0530

    Refactored buyer model

--- buyer_data/models.py
+++ buyer_data/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from common.models import CustomUser, UserExtensionMixin, VerificationMixin
 
 # Create your models here.
-class Buyer(User, UserExtensionMixin):
+class Buyer(UserExtensionMixin):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='buyer_profile')
 
-    def __str__(self): return f"{self.first_name} {self.last_name}"
+    def __str__(self): return f"{self.user.first_name} {self.user.last_name}"
 
 
 class BuyerVerification(VerificationMixin):
     buyer = models.OneToOneField(Buyer, on_delete=models.CASCADE)
 
-    def __str__(self): return f"{self.buyer.first_name} {self.buyer.last_name}"
+    def __str__(self): return f"{self.buyer.user.first_name} {self.buyer.user.last_name}"
