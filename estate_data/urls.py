300147f4b5bd5fc85f6b557c739ddf070de754d9
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-05-10 20:41:40 +0530

    Corrected route

--- estate_data/urls.py
+++ estate_data/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('estate-details/<str:estate_slug>', views.get_estate_data, name='estate_details'),
     path('add-new-estate', views.add_new_estate, name='add_new_estate'),
     path('update-estate/<str:slug>', views.update_estate_data, name='update_estate_data'),
-    path('area', cache_page(60*60)(views.EstateAreaView.as_view()), name='estate_area'),
+    path('area', views.area_estate, name='estate_area'),
 ]
