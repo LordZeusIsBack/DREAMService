fc9218983ab0823bd206ea253ae580128e9e075e
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-05-14 00:07:11 +0530

    Changed allowed hosts

--- dream_service/settings.py
+++ dream_service/settings.py
@@ -25,9 +25,9 @@ load_dotenv()
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
