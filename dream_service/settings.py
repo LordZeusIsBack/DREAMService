4b7c3aabbadbfa381dcae45380843268be7b7bc2
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-05-14 00:05:51 +0530

    Moved information to environment files

--- dream_service/settings.py
+++ dream_service/settings.py
@@ -87,9 +87,9 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv('DB_NAME'),
         "USER": os.getenv('DB_USER'),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": "5432",
+        "PASSWORD": os.getenv('DB_PASSWORD'),
+        "HOST": os.getenv('DB_HOST'),
+        "PORT": os.getenv('DB_PORT'),
         "OPTIONS": {
             "sslmode": "require",
         },
