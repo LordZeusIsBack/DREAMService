4a4f0d6d8d00634d96179687005e000dee2ef166
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-05-14 15:06:46 +0530

    Custom Manager Added

--- common/models.py
+++ common/models.py
@@ -3,6 +3,42 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email: raise ValueError('Users must have an email address')
+        if not password: raise ValueError('Users must have a password')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True: raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True: raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, username, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=30, unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
 class UserExtensionMixin(models.Model):
     is_deleted = models.BooleanField(default=False)
     profile_picture = models.ImageField(upload_to='pictures/seller', null=True, blank=True)
