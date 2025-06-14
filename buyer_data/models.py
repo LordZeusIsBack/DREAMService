bdc5ad1ad284843ad4dcb538fd369071899c51f1
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-06-15 03:35:40 +0530

    Added new table for wishlisting estates

--- buyer_data/models.py
+++ buyer_data/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from common.models import CustomUser, UserExtensionMixin, VerificationMixin
+from estate_data.models import Estate
 
 # Create your models here.
 class Buyer(UserExtensionMixin):
@@ -12,3 +13,18 @@ class BuyerVerification(VerificationMixin):
     buyer = models.OneToOneField(Buyer, on_delete=models.CASCADE, related_name='buyerverification')
 
     def __str__(self): return f"{self.buyer.user.first_name} {self.buyer.user.last_name}"
+
+class WishlistItem(models.Model):
+    buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE, related_name='wishlistitems')
+    estate = models.ForeignKey(Estate, on_delete=models.CASCADE, related_name='wishlisted_by')
+    added_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('buyer', 'estate')
+        ordering = ['-added_on']
+        indexes = [
+            models.Index(fields=['buyer', 'added_on']),
+            models.Index(fields=['estate', 'added_on'])
+        ]
+
+    def __str__(self): return f"{self.buyer.user.email} bookmarked {self.estate.estate_name}"
