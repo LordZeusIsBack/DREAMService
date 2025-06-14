972f329d2071c1c7cea0fb5a2723e0172762f604
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-06-15 01:15:18 +0530

    Changed Allowed hosts

--- dream_service/settings.py
+++ dream_service/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['dreamservice.onrender.com']
+ALLOWED_HOSTS = ['dream-service.vercel.app', 'dreamservice.onrender.com']
 
 
 # Application definition
