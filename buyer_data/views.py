12b2cbaf1c76323218f68abb26b61dfb414c362b
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-06-16 15:41:01 +0530

    Added Necessary imports

--- buyer_data/views.py
+++ buyer_data/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
+from django.db import transaction
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 import buyer_data.models as buyer_models
 from buyer_data.serializer import BuyerSerializer, PurchasedEstateSerializer
 from rest_framework.response import Response
