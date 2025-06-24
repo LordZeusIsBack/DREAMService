1d0a3ceb83cc9f84322d3d81426501b789b0ca96
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-06-24 16:41:02 +0530

    Added the tables to the admin panel

--- estate_data/admin.py
+++ estate_data/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import EstateImage, Estate
+from django.utils.html import format_html
 
 # Register your models here.
+@admin.register(EstateImage)
+class EstateAdmin(admin.ModelAdmin):
+    list_display = ['estate', 'image_preview']
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="100" style="object-fit:contain;" />', obj.image.url)
+        return "No Image"
+
+    image_preview.short_description = 'Image Preview'
+
+@admin.register(Estate)
+class EstateAdmin(admin.ModelAdmin):
+    list_display = ['estate_name', 'estate_type', 'estate_price', 'status', 'latitude', 'longitude', 'slug', 'created_at', 'updated_at']
