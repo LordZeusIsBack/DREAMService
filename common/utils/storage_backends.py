ef561aa63e1d9cd9db3a859cfbc70912de8200a3
Anubhav Sharma <anubhavsharma5645@gmail.com>
2025-06-25 13:57:17 +0530

    Removed unnecessary parameter

--- common/utils/storage_backends.py
+++ common/utils/storage_backends.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
-load_dotenv()
 class MediaStorage(S3Boto3Storage):
     default_acl = None
     file_overwrite = False
