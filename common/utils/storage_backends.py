7819e8b9a2668d82218580d36c17943fd1919872
Anubhav Sharma <anubhavsharma5645@gmail.com>
2025-06-25 13:55:04 +0530

    Update storage_backends.py

--- common/utils/storage_backends.py
+++ common/utils/storage_backends.py
@@ -1,9 +1,11 @@
+from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
+load_dotenv()
 class MediaStorage(S3Boto3Storage):
     default_acl = None
     file_overwrite = False
     querystring_auth = True
     querystring_expire = 900
     location = 'pictures/'
-    bucket_name = 'testing-dreamy-bucket'
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
