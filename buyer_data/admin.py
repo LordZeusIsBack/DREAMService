0b05f63ce92fe5f8067ac16a08c836f90b268be4
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-06-27 02:05:40 +0530

    Added admin for Buyer model

--- buyer_data/admin.py
+++ buyer_data/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    readonly_fields = ('phone_number',)
+    list_display = ('user', 'user__first_name', 'user__last_name', 'phone_number')
+    search_fields = ('user__username', 'phone_number')
+
 @admin.register(PurchasedEstate)
 class BuyerPurchaseAdmin(admin.ModelAdmin):
     readonly_fields = ('purchase_date', 'transaction_id')
