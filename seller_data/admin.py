afc9330585baca99aee2faacdf441eb123879cee
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-06-27 02:22:09 +0530

    Added admin for Seller Model

--- seller_data/admin.py
+++ seller_data/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Seller)
+class SellerAdmin(admin.ModelAdmin):
+    readonly_fields = ('business_name', 'phone_number')
+    list_display = ('seller_username', 'seller_email', 'business_name', 'phone_number')
+
+    @admin.display(description='Seller Email')
+    def seller_email(self, obj):
+        return obj.user.email
+
+    @admin.display(description='Seller Username')
+    def seller_username(self, obj):
+        return obj.user.username
