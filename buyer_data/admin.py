7ce0cb80d122ead6dec184fb828e77cf5ecd0a23
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-06-27 02:06:18 +0530

    Customized admin class

--- buyer_data/admin.py
+++ buyer_data/admin.py
@@ -5,8 +5,24 @@ from .models import *
 @admin.register(Buyer)
 class BuyerAdmin(admin.ModelAdmin):
     readonly_fields = ('phone_number',)
-    list_display = ('user', 'user__first_name', 'user__last_name', 'phone_number')
-    search_fields = ('user__username', 'phone_number')
+    list_display = ('user_email', 'first_name', 'last_name', 'username', 'phone_number')
+    search_fields = ('user__email', 'phone_number', 'user__username')
+
+    @admin.display(description='Email')
+    def user_email(self, obj):
+        return obj.user.email
+
+    @admin.display(description='First Name')
+    def first_name(self, obj):
+        return obj.user.first_name
+
+    @admin.display(description='Last Name')
+    def last_name(self, obj):
+        return obj.user.last_name
+
+    @admin.display(description='Username')
+    def username(self, obj):
+        return obj.user.username
 
 @admin.register(PurchasedEstate)
 class BuyerPurchaseAdmin(admin.ModelAdmin):
