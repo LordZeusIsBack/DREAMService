05bcccc10af4d4f4636940fe677b6fda9db86d64
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-06-29 01:14:50 +0530

    Added CORS setup

--- dream_service/settings.py
+++ dream_service/settings.py
@@ -46,19 +46,24 @@ INSTALLED_APPS = [
     "buyer_data",
     "common",
     "estate_data",
-    "storages"
+    "storages",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = ["https://dreamservice.com"]
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "dream_service.urls"
 
