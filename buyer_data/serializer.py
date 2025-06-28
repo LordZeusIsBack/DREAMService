f139e93ccd74afd4cd8c3e44b4fe176abbef645f
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-06-28 15:11:34 +0530

    Corrected Multipart nesting for buyer

--- buyer_data/serializer.py
+++ buyer_data/serializer.py
@@ -20,7 +20,10 @@ class BuyerSerializer(BaseUserSerializer):
     """
     Serializer for buyer model.
     """
-    verification = BuyerVerificationSerializer(source='buyerverification', read_only=False)
+    aadhaar_number = serializers.IntegerField(write_only=True)
+    pan_number = serializers.CharField(write_only=True)
+    aadhaar_card = serializers.ImageField(required=False, write_only=True)
+    pan_card = serializers.ImageField(required=False, write_only=True)
     first_name = serializers.CharField(source='user.first_name')
     last_name = serializers.CharField(source='user.last_name')
     email = serializers.EmailField(source='user.email')
@@ -28,7 +31,8 @@ class BuyerSerializer(BaseUserSerializer):
     profile_picture = serializers.ImageField(required=False)
     class Meta(BaseUserSerializer.Meta):
         model = models.Buyer
-        fields = ('id', 'first_name', 'last_name', 'email', 'phone_number', 'username', 'password', 'verification', 'profile_picture')
+        fields = ('id', 'first_name', 'last_name', 'email', 'phone_number', 'username', 'password', 'aadhaar_number',
+                  'pan_number', 'aadhaar_card', 'pan_card', 'profile_picture', 'profile_picture')
 
     def create(self, validated_data):
         """
@@ -37,6 +41,13 @@ class BuyerSerializer(BaseUserSerializer):
         Returns:
             Buyer: The newly created buyer instance.
         """
+        verification_data = {
+            'aadhaar_number': validated_data.pop('aadhaar_number'),
+            'pan_number': validated_data.pop('pan_number'),
+            'aadhaar_card': validated_data.pop('aadhaar_card', None),
+            'pan_card': validated_data.pop('pan_card', None),
+        }
+        validated_data['buyerverification'] = verification_data
         return BaseUserSerializer.create_user(
             validated_data,
             models.Buyer,
