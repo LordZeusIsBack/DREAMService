d9fabb8e7e6d1914c3f4a65fe3b62f10a4e7ac2f
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-06-30 04:06:20 +0530

    Added a new field for government issued ID

--- estate_data/models.py
+++ estate_data/models.py
@@ -24,6 +24,7 @@ def estate_image_path(instance, filename):
 # Create your models here.
 class Estate(models.Model):
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    estate_government_id = models.CharField(max_length=255, unique=True, db_index=True)
     estate_name = models.CharField(max_length=255)
     estate_type = models.CharField(max_length=255, choices=[
         ('apartment', 'Apartment'),
