7e1ebf0c52f35d46ebac26abedc28c69c1e9ebca
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-07-07 00:56:17 +0530

    Added Pagination Logic

--- dream_service/settings.py
+++ dream_service/settings.py
@@ -115,6 +115,11 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 15
+}
+
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
