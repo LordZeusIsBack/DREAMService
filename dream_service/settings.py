12b134544c8a18d62161ccb9f4d247a9207bba78
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-07-10 13:46:52 +0530

    Corrected Django Redis backend connection

--- dream_service/settings.py
+++ dream_service/settings.py
@@ -122,14 +122,14 @@ REST_FRAMEWORK = {
 
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": os.getenv('REDIS_URL_DEFAULT'),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
         },
     },
     "views_ip_cache": {
-        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": os.getenv('REDIS_URL_IP_CACHE'),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
