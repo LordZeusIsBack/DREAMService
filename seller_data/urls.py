fcb5287d9f3d1d95977320eed5cf9073d66f695a
Anubhav Sharma <anubhavsharma@dypakurdi.onmicrosoft.com>
2025-07-11 11:45:13 +0530

    Update urls.py

--- seller_data/urls.py
+++ seller_data/urls.py
@@ -4,3 +4,7 @@ from common.urls import create_user_url_patterns
 
 urlpatterns = create_user_url_patterns(views, 'seller') + [path('get_listed_estates/<str:seller_username>',
                                                                views.get_listed_estates, name='seller_estate_data')]
+
+urlpatterns = create_user_url_patterns(views, 'seller') + [
+    path('get_listed_estates/<str:seller_username>', views.get_listed_estates, name='seller_estate_data')
+]
