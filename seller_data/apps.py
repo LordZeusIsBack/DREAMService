38d03760d0390afa0cb45c05a44792a23632cad5
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-07-15 02:34:40 +0530

    Connected signals to app routing

--- seller_data/apps.py
+++ seller_data/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SellerDataConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "seller_data"
+
+    def ready(self):
+        from .utils import signals
