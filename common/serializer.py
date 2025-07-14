6cfaafd5b20208326ce46b71886c931e37b7ac43
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-07-14 12:51:22 +0530

    Uncommented send_otp

--- common/serializer.py
+++ common/serializer.py
@@ -49,7 +49,7 @@ class BaseUserSerializer(serializers.ModelSerializer):
                     first_name=user_data['first_name'],
                     last_name=user_data['last_name']
                 )
-                # send_otp(custom_user_instance.email, is_resend=False)
+                send_otp(custom_user_instance.email, is_resend=False)
                 user_model_instance = user_model.objects.create(user=custom_user_instance, **validated_data)
                 try: user_model_instance.full_clean()
                 except Exception as e: raise ValidationError({'error': str(e)})
