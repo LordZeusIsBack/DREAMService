b91bbc751ac9acff7141080308dfcd463448aadc
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-07-15 15:36:08 +0530

    Corrected URL route to take generalized parameter

--- common/urls.py
+++ common/urls.py
@@ -18,7 +18,7 @@ def create_user_url_patterns(view_module, user_type):
         path(f'view-{user_type}/<str:{user_type}_username>', getattr(view_module, f'{user_type}_data'), name=view_name),
         path(f'update-{user_type}/<str:username>', getattr(view_module, f'update_{user_type}_data'), name=update_name),
         path(f'add-{user_type}', getattr(view_module, f'add_{user_type}'), name=f'add_{user_type}'),
-        path(f'delete-{user_type}/<str:{user_type}_username>', getattr(view_module, f'delete_{user_type}'), name=f'delete_{user_type}'),
+        path(f'delete-{user_type}/<str:username>', getattr(view_module, f'delete_{user_type}'), name=f'delete_{user_type}'),
         path('forgot-password/', getattr(view_module, f'{user_type}_forgot_password'), name=forgot_name),
         path('reset-password/', getattr(view_module, f'{user_type}_reset_password'), name=reset_name),
         path(f'{user_type}-login/', getattr(view_module, f'{user_type}_login'), name=f'{user_type}_login'),
