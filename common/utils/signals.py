0c580e7e942e787937fc6e31c83de93abaa24252
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-07-15 11:50:08 +0530

    Added a common signal to delete verification documents of user

--- common/utils/signals.py
+++ common/utils/signals.py
@@ -2,11 +2,17 @@ from django.core.files.storage import default_storage
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
-from buyer_data.models import Buyer
-from seller_data.models import Seller
+from buyer_data.models import Buyer, BuyerVerification
+from seller_data.models import Seller, SellerVerification
 
 
 @receiver(post_delete, sender=Buyer)
 @receiver(post_delete, sender=Seller)
 def delete_user_profile_picture(sender, instance, **kwargs):
     if instance.profile_picture and default_storage.exists(instance.profile_picture.name): default_storage.delete(instance.profile_picture.name)
+
+@receiver(post_delete, sender=BuyerVerification)
+@receiver(post_delete, sender=SellerVerification)
+def delete_user_verification_documents(sender, instance, **kwargs):
+    if instance.aadhaar_card and default_storage.exists(instance.aadhaar_card.name): default_storage.delete(instance.aadhaar_card.name)
+    if instance.pan_card and default_storage.exists(instance.pan_card.name): default_storage.delete(instance.pan_card.name)
