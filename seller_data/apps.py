98c77304a7a6789ef8fc57e959b508d541098b39
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-07-15 11:43:19 +0530

    Removed signals utils file for seller

--- seller_data/apps.py
+++ seller_data/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class SellerDataConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "seller_data"
-
-    def ready(self):
-        from .utils import signals
