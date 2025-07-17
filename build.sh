ea68d32b411b47888d0610c56df464c423ba030f
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-07-17 13:55:25 +0530

    Added commands to create temporary storage point for logs

--- build.sh
+++ build.sh
@@ -2,3 +2,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+mkdir -p logs/buyer_logs
+mkdir -p logs/seller_logs
+mkdir -p logs/estate_logs
+mkdir -p logs/common_logs
