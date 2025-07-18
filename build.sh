2715e5bcd6df86373167d0cb1cb7e70010b7be1e
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-07-18 12:23:24 +0530

    Corrected directory nomenclature

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 set -o errexit
 pip install -r requirements.txt
-mkdir -p logs/buyer_logs
-mkdir -p logs/seller_logs
-mkdir -p logs/estate_logs
+mkdir -p logs/buyer_data_logs
+mkdir -p logs/seller_data_logs
+mkdir -p logs/estate_data_logs
 mkdir -p logs/common_logs
 chmod 755 logs
 chmod 755 logs/*_logs
