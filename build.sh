0345374621267a04b5d169e42f8a3e415cb6b355
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-07-18 12:18:01 +0530

    Changed sequence number of commands

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 set -o errexit
 pip install -r requirements.txt
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
 mkdir -p logs/buyer_logs
 mkdir -p logs/seller_logs
 mkdir -p logs/estate_logs
 mkdir -p logs/common_logs
 chmod 755 logs
 chmod 755 logs/*_logs
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
 echo "Build completed successfully!"
