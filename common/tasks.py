c2ab74bcba6840eea63949819fc9b79888aa2498
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-07-19 15:46:58 +0530

    Corrected parameter

--- common/tasks.py
+++ common/tasks.py
@@ -4,5 +4,5 @@ from common.utils.upload_logs import upload_logs_now
 
 
 @shared_task(bind=True, autoretry_for=(Exception,), retry_kwargs={'max_retries': 3, 'countdown': 60})
-def upload_to_s3(self):
+def upload_to_s3():
     return upload_logs_now()
