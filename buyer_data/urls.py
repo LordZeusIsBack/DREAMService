b4a99579b9124f21d451edd2000563cd19c475cc
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-07-21 15:24:14 +0530

    Added route for the affordability calculator GET request

--- buyer_data/urls.py
+++ buyer_data/urls.py
@@ -1,4 +1,7 @@
+from django.urls import path
 import buyer_data.views as views
 from common.urls import create_user_url_patterns
 
-urlpatterns = create_user_url_patterns(views, 'buyer')
+urlpatterns = create_user_url_patterns(views, 'buyer') + [
+    path('affordability-calculator', views.affordability_calculator, name='affordability_calculator')
+]
