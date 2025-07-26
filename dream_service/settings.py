59870ef5d0c6c464714e629af6c601383d3231b8
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-07-26 23:36:53 +0530

    Removed default authentication classes setup

--- dream_service/settings.py
+++ dream_service/settings.py
@@ -191,10 +191,7 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 15,
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.permissions.IsAdminUser"
-    ]
+    "PAGE_SIZE": 15
 }
 
 CACHES = {
