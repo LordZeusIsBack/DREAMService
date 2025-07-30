5df04003d52305fc515d884ab935423dd166ce8a
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-07-30 12:43:13 +0530

    Corrected the route to accept username as part of URL

--- buyer_data/urls.py
+++ buyer_data/urls.py
@@ -6,6 +6,6 @@ urlpatterns = create_user_url_patterns(views, 'buyer') + [
     path('eligibility-calculator', views.eligibility_calculator, name='eligibility_calculator'),
     path('emi-calculator', views.emi_calculator, name='emi_calculator'),
     path('affordability-calculator', views.affordability_calculator, name='affordability_calculator'),
-    path('add-to-wishlist', views.add_bookmarks, name='add_bookmarks'),
+    path('add-to-wishlist/<str:buyer_username>', views.add_bookmarks, name='add_bookmarks'),
     path('get-bookmarks/<str:buyer_username>', views.bookmarked_estates, name='get_bookmarks'),
 ]
