4463f76087466cde801ce20aa8478a82ab286843
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-07-30 12:44:57 +0530

    Added a URL route to connect to the FBV related to remove bookmark

--- buyer_data/urls.py
+++ buyer_data/urls.py
@@ -7,5 +7,6 @@ urlpatterns = create_user_url_patterns(views, 'buyer') + [
     path('emi-calculator', views.emi_calculator, name='emi_calculator'),
     path('affordability-calculator', views.affordability_calculator, name='affordability_calculator'),
     path('add-to-wishlist/<str:buyer_username>', views.add_bookmarks, name='add_bookmarks'),
+    path('remove-bookmark/<str:buyer_username>', views.remove_bookmarks, name='remove_bookmarks'),
     path('get-bookmarks/<str:buyer_username>', views.bookmarked_estates, name='get_bookmarks'),
 ]
